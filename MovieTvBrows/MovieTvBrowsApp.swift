852f3cd28435d952fbb1bbb55d8524b091dd365c
ROHAN SINGH BHOJAK <bhojakrohansingh@gmail.com>
2025-11-12 12:58:36 +0530

    Initial Commit

---
+++ MovieTvBrows/MovieTvBrowsApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieTvBrowsApp.swift
+//  MovieTvBrows
+//
+//  Created by Rohan Bhojak  on 12/11/25.
+//
+
+import SwiftUI
+
+@main
+struct MovieTvBrowsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
